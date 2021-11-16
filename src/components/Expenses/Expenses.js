1d290fa0e54d004a4f64de87752d0a3d965bf838
DJ Corbett <dcorbett@yext.com>
2021-11-15 23:33:51 -0500

    passing information to parent components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,30 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+    
     return (
-        <Card className="expenses">
-            {props.list.map(expense => (
-                <ExpenseItem 
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />
-            ))}
-        </Card>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {props.list.map(expense => (
+                    <ExpenseItem 
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                        key={expense.id}
+                    />
+                ))}
+            </Card>
     )
 }
 
